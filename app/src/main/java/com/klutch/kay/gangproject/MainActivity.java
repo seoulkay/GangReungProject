c1ac94b11ad0fd10497f37912fe37e81c4d9a4cd
Kay <seoulkay@gmail.com>
2015-11-19 02:16:52 +0900

    그래들 왜 에러가날까요..

--- app/src/main/java/com/klutch/kay/gangproject/MainActivity.java
+++ app/src/main/java/com/klutch/kay/gangproject/MainActivity.java
@@ -1,10 +1,12 @@
 package com.klutch.kay.gangproject;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,11 +16,14 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,9 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> m_Adapter;
 
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -45,8 +49,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //파일이름으로 제이슨 객체 받기
-        JSONObject jobj = loadJSONFromAsset("skiGolf.json");
-
+        try {
+            JSONObject jobj = new JSONObject(loadJSONFromAsset());
+        }catch(JSONException je){
+            Log.d("JSON Error", je.toString());
+        }
         // Android에서 제공하는 string 문자열 하나를 출력 가능한 layout으로 어댑터 생성
         m_Adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1);
 
@@ -60,13 +67,8 @@ public class MainActivity extends AppCompatActivity {
         m_ListView.setOnItemClickListener(onClickListItem);
 
         // ListView에 아이템 추가
-        m_Adapter.add("하스스톤");
-        m_Adapter.add("몬스터 헌터");
-        m_Adapter.add("디아블로");
-        m_Adapter.add("와우");
-        m_Adapter.add("리니지");
-        m_Adapter.add("안드로이드");
-        m_Adapter.add("아이폰");
+        m_Adapter.add("강릉아이템1");
+        //m_Adapter.add(jsonObject+ "");
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -75,7 +77,9 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "버튼을 눌렀습니다", Toast.LENGTH_LONG).show();
             }
         });
+
     }
+
     // 아이템 터치 이벤트
     private AdapterView.OnItemClickListener onClickListItem = new AdapterView.OnItemClickListener() {
 
@@ -86,8 +90,8 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    public void ClickButton(View v){
-        Toast.makeText(getApplicationContext(),"버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
+    public void ClickButton(View v) {
+        Toast.makeText(getApplicationContext(), "버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -112,31 +116,28 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public JSONObject loadJSONFromAsset(String fileName){
+    public String loadJSONFromAsset() {
         String json = null;
+        try {
 
-        try{
-            InputStream is = getAssets().open(fileName);
+            InputStream is = getAssets().open("skiGolf.json");
 
             int size = is.available();
+
             byte[] buffer = new byte[size];
+
             is.read(buffer);
+
             is.close();
+
             json = new String(buffer, "UTF-8");
 
-        }catch(IOException ex){
+
+        } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
+        return json;
 
-        try {
-            JSONObject jsonObj = new JSONObject(json);
-            return jsonObj;
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
-
 }
